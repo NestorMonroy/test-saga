f0715977f56b080be24f1202e417831c4e3fd161
NestorMonroy <nestor.monroy.90@gmail.com>
2020-09-27 14:07:15 -0500

    example take dalay put redux-saga

--- src/redux/app.saga.js
+++ src/redux/app.saga.js
@@ -1,6 +1,20 @@
-import { take } from 'redux-saga/effects';
+import { takeLatest, take, delay, put } from 'redux-saga/effects';
+
+export function* onIncrement(){
+    yield console.log('I am incremented');
+    yield delay(3000);
+    yield put({ type: 'INCREMENT_FROM_SAGA'});
+}
 
 export function* incrementSaga(){
-    yield take('INCREMENT');
-    console.log('I am incremented');
+
+
+    
+/*     while (true) {
+        yield take('INCREMENT');
+        console.log('I am incremented');
+        yield delay(5000);
+    } */
+
+    yield takeLatest('INCREMENT', onIncrement );
 }
